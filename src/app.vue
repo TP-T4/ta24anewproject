19a67243542186580da7deb3bacbccdc01e8ed7a
TP-T4 <Tauri.Parli@tptlive.ee>
2026-01-28 13:37:54 +0200

    a

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,67 @@
 <script setup>
-    let message = 'Hello, Vue 3!';
+import { ref, computed } from 'vue'
+import ItemList from './ItemList.vue'
+ 
+let id = 1
+ 
+const items = ref([
+  { id: id++, text: 'Sai', isDone: true },
+  { id: id++, text: 'Piim', isDone: false },
+  { id: id++, text: 'Viin', isDone: true },
+  { id: id++, text: 'Õlu', isDone: false }
+])
+ 
+const newItem = ref('')
+ 
+const doneItems = computed(() =>
+  items.value.filter(item => item.isDone)
+)
+ 
+const toDoItems = computed(() =>
+  items.value.filter(item => !item.isDone)
+)
+ 
+function add() {
+  const text = newItem.value.trim()
+  if (!text) return
+ 
+  items.value.push({
+    id: id++,
+    text,
+    isDone: false
+  })
+ 
+  newItem.value = ''
+}
 </script>
-
+ 
 <template>
-    <h1>{{ message }}</h1>
+  <div class="container">
+    <div class="field has-addons mt-2">
+      <div class="control is-expanded">
+        <input
+          class="input"
+          type="text"
+          v-model="newItem"
+          @keypress.enter="add"
+        />
+      </div>
+ 
+      <div class="control">
+        <button class="button is-info" @click="add">
+          Add Item
+        </button>
+      </div>
+    </div>
+    <div class="content">
+        <ItemList :items="items" title="All Items"></ItemList>
+        <ItemList :items="doneItems" title="Done Items"></ItemList>
+        <ItemList :items="toDoItems" title="Todo Items"></ItemList>
+    </div>
+  </div>
 </template>
-
+ 
 <style>
-
 </style>
+ 
+ 
---
+++ src/components/ItemList.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['items'])
+</script>
+<template>
+    <h1>All Items</h1>
+            <ul>
+                <li v-for="item in items" :key="item.id">
+                    {{ item.text }}
+                    <input type="checkbox" v-model="item.isDone">
+                </li>
+            </ul>
+
+</template>
