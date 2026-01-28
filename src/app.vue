90a2044f163f69993e32fe93023725914a81f8ff
TP-T4 <Tauri.Parli@tptlive.ee>
2026-01-28 14:08:50 +0200

    ADD MODAL

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import Modal from './components/Modal.vue';
 
 let ModalActive = ref(false);
 
@@ -9,15 +10,8 @@ let ModalActive = ref(false);
 <template>
   <div class="container">
     <button class="button is-primary" @click="ModalActive=true">Open Modal</button>
-    <div :class="ModalActive ? 'modal is-active' : 'modal'">
-      <div class="modal-background" @click="ModalActive=false"></div>
-      <div class="modal-content">
-        <p class="image is-4by3">
-          <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
-        </p>
-      </div>
-      <button class="modal-close is-large" aria-label="close" @click="ModalActive=false"></button>
-    </div>
+    
+    <Modal :active="ModalActive"></Modal>
   </div>
 </template>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['active']);
+</script>
+
+<template>
+    <div :class="ModalActive ? 'modal is-active' : 'modal'">
+      <div class="modal-background" @click="ModalActive=false"></div>
+      <div class="modal-content">
+        <p class="image is-4by3">
+          <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
+        </p>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @close="ModalActive=false"></button>
+    </div>
+</template>
