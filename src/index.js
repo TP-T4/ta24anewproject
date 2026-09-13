c841fef1122d6c1d88838037fb80569b2775da9f
Tauri <tauri.parli@tptlive.ee>
2026-09-13 14:20:57 +0300

    ChuckNorris API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
-import App from './app.vue';
-import ToDo from './pages/ToDo.vue';
-import Modals from './pages/Modals.vue';
-import Messages from './pages/messages.vue';
+import { createApp } from 'vue'
+
+import App from './app.vue'
+import ToDo from './pages/ToDo.vue'
+import Modals from './pages/Modals.vue'
+import Messages from './pages/messages.vue'
+import Validation from './pages/Validation.vue'
+import ChuckNorris from './pages/ChuckNorris.vue'
+
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', component: ToDo },
-  { path: '/modals', component: Modals },
-  { path: '/messages', component: Messages },
-];
+  { path: '/', component: ToDo, name: 'ToDo' },
+  { path: '/modals', component: Modals, name: 'Modals' },
+  { path: '/messages', component: Messages, name: 'Messages' },
+  { path: '/validation', component: Validation, name: 'Validation' },
+  { path: '/validation', component: Validation, name: 'Button' },
+  { path: '/chucknorris', component: ChuckNorris, name: 'Chuck' },
+]
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHashHistory(),
   routes,
-});
+})
 
-createApp(App).use(router).mount('#app');
+
+
+const app = createApp(App)
+app.use(router)
