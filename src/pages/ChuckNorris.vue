d52a2d434e2acc0656fd50622151d0ac273f21a5
Tauri <tauri.parli@tptlive.ee>
2026-09-13 14:41:04 +0300

    Rick and Morty updated

--- src/pages/ChuckNorris.vue
+++ src/pages/ChuckNorris.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue';
 
 let joke = ref('')
 
+import axios from 'axios'
+
 const res = await axios.get("https://api.chucknorris.io/jokes/random")
         joke.value = res.data.value
         joke.value = data.value
