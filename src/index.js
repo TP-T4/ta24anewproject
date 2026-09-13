65bffac2a57e88ea33998d2b3a10d1a71747c672
Tauri <tauri.parli@tptlive.ee>
2026-09-13 14:50:30 +0300

    Canvas added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Messages from './pages/messages.vue'
 import Validation from './pages/Validation.vue'
 import ChuckNorris from './pages/ChuckNorris.vue'
 import RickandMorty from './pages/RickandMorty.vue'
+import CanvasExample from './pages/CanvasExample.vue'
 
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
@@ -14,10 +15,10 @@ const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals' },
   { path: '/messages', component: Messages, name: 'Messages' },
-  { path: '/validation', component: Validation, name: 'Validation' },
-  { path: '/validation', component: Validation, name: 'Button' },
+  { path: '/button', component: Validation, name: 'Button' },
   { path: '/chucknorris', component: ChuckNorris, name: 'Chuck' },
   { path: '/rickandmorty', component: RickandMorty, name: 'Rick and Morty' },
+  { path: '/canvas', component: CanvasExample, name: 'Canvas Example' }
 ]
 
 export const router = createRouter({
